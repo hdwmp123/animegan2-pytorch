8cd2df040eba26aeacae414bfa5111357652f74c
hdwmp123 <hdwmp123@163.com>
2023-05-16 16:17:13 +0800

    1 代码优化

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ from torchvision.transforms.functional import to_tensor, to_pil_image
 
 from model import Generator
 
-
 torch.backends.cudnn.enabled = False
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
@@ -21,6 +20,7 @@ def load_image(image_path, x32=False):
     if x32:
         def to_32s(x):
             return 256 if x < 256 else x - x % 32
+
         w, h = img.size
         img = img.resize((to_32s(w), to_32s(h)))
 
@@ -29,18 +29,18 @@ def load_image(image_path, x32=False):
 
 def test(args):
     device = args.device
-    
+
     net = Generator()
     net.load_state_dict(torch.load(args.checkpoint, map_location="cpu"))
     net.to(device).eval()
     print(f"model loaded: {args.checkpoint}")
-    
+
     os.makedirs(args.output_dir, exist_ok=True)
 
     for image_name in sorted(os.listdir(args.input_dir)):
         if os.path.splitext(image_name)[-1].lower() not in [".jpg", ".png", ".bmp", ".tiff"]:
             continue
-            
+
         image = load_image(os.path.join(args.input_dir, image_name), args.x32)
 
         with torch.no_grad():
@@ -54,7 +54,6 @@ def test(args):
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--checkpoint',
@@ -62,13 +61,13 @@ if __name__ == '__main__':
         default='./weights/paprika.pt',
     )
     parser.add_argument(
-        '--input_dir', 
-        type=str, 
+        '--input_dir',
+        type=str,
         default='./samples/inputs',
     )
     parser.add_argument(
         '--output_dir',
-        type=str, 
+        type=str,
         default='./samples/results',
     )
     parser.add_argument(
@@ -88,5 +87,5 @@ if __name__ == '__main__':
         help="Resize images to multiple of 32"
     )
     args = parser.parse_args()
-    
+
     test(args)
