414c3e318bca99c8b74caaa1124acedffa1520cd
hdwmp123 <hdwmp123@163.com>
2023-05-16 15:30:39 +0800

    1 修改参数

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:1024'
+export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512'
 python test.py \
   --checkpoint ./weights/paprika.pt \
   --input_dir ./samples/faces \
